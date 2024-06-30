c75524930d8ab1a6dda5e6194312e71a470fa5d7
adios-kelly <kelly514wang@gmail.com>
2024-06-30 20:53:18 +0800

    add simple calc webpage.

--- app.py
+++ app.py
@@ -6,6 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/simple_calc')
+def simple_calc():
+    return render_template('simple_calc.html')
 
     
 if __name__ == '__main__':
