eaed0c3b873bf1b92ec8bde50a1c506f8d656ac4
adios-kelly <kelly514wang@gmail.com>
2024-08-18 16:35:35 +0800

    calc std value for input list.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 from flask import request
-from mcalc import m_simple_calc
+from mcalc import m_simple_calc,m_std_calc
 
 
 app = Flask(__name__)
@@ -12,6 +12,10 @@ def index():
 @app.route('/simple_calc', methods=['POST','GET'])
 def simple_calc():
     return m_simple_calc()
+
+@app.route('/std_calc', methods=['POST','GET'])
+def std_calc():
+    return m_std_calc()
 
     
 if __name__ == '__main__':
--- mcalc.py
+++ mcalc.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template
 from flask import request
+import numpy as np
+
 
 def m_simple_calc():
     data_min_value_tmp = -1.234
@@ -17,3 +19,21 @@ def m_simple_calc():
                             data_min_value=data_min_value_tmp)
 
 
+
+def mstd(arr):
+    return np.std(arr)
+
+def m_std_calc():
+    data_std_value_tmp = -1.234
+
+    if request.method == 'POST':
+        datatxt = request.form.get('datatxt')
+        data_list = []
+        for x in datatxt.split(','):
+            x = x.strip()
+            x = float(x)
+            data_list.append(x)
+        print('data_list_input={0}'.format(data_list))
+        data_std_value_tmp = mstd(data_list)
+    return render_template('std_calc.html',
+                            data_std_value=data_std_value_tmp)
